3d55624955edf44d4d23fa8cf0d02e0e5b9e072c
Andrew Davidson <andrew.davidson.digital@gmail.com>
2024-09-05 12:27:25 +1000

    Structure:  - Add some CSS reset  - Add grid-stack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +9,20 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      // resolve to : `grid [grid-template-areas:_"stack"] *:[grid-area:_stack]`
+      const stackableGridArea = {
+        '.grid-area-stack' : {
+          display: 'grid',
+          gridTemplateAreas: '"stack"',
+          '>*' : {
+            'grid-area': 'stack',
+          }          
+        }
+      }
+      addUtilities(stackableGridArea);
+    }),
+  ],
 }
 
